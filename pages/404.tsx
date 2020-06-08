df69699b8c5fc4614f399f4a6cd6d69a7c964bd8
Simon Kienzler <SimonKienzler@users.noreply.github.com>
2020-06-08 17:17:13 +0200

    [Impl] Add custom 404 error page

---
+++ pages/404.tsx
@@ -0,0 +1,18 @@
+import { GetStaticProps } from 'next'
+import MarkdownLoader from '../components/util/markdown-loader'
+import StaticPage from '../components/static-page'
+
+function Page({content, data}) {
+    return (
+        <StaticPage
+            title={data.title}
+            content={content}
+        />
+    )
+}
+
+export default Page
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    return MarkdownLoader.single('pages', '404')
+}
