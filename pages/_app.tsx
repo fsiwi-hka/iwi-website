5c1e51f0bd584d5a009de63706f612570dd5fb67
Simon Kienzler <SimonKienzler@users.noreply.github.com>
2020-06-23 16:52:23 +0200

    [Impl] Add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,6 +22,11 @@ function IWIWebsite({ Component, pageProps }) {
             <meta property="og:description" content={og ? og.description : `Die Website der Fachschaft IWI, Hochschule Karlsruhe (HKA)`} />
             <meta name="twitter:site" content="@fsi_hska" />
 
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32" />
+            <link rel="icon" type="image/png" href="/favicon.png" sizes="96x96" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+
             <link href="https://fonts.googleapis.com/css2?family=Muli:wght@400;700&family=Roboto:wght@300;500&display=swap" rel="stylesheet"></link>
             <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" ></link>
 
