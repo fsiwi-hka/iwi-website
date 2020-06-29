7b673629487febdaaeab25ae38f55967cf7bb69b
Simon Kienzler <SimonKienzler@users.noreply.github.com>
2020-06-29 09:20:12 +0200

    [Impl] Outsource page head to own component

---
+++ components/common/page-head.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+
+function PageHead({ title, og }) {
+    return (
+        <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta property="og:title" content={title || `Fachschaft IWI`} />
+            <meta property="og:site_name" content="Fachschaft IWI" />
+            <meta property="og:description" content={og ? og.description : `Die Website der Fachschaft IWI, Hochschule Karlsruhe (HKA)`} />
+            <meta name="twitter:site" content="@fsi_hska" />
+
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32" />
+            <link rel="icon" type="image/png" href="/favicon.png" sizes="96x96" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+
+            <link href="https://fonts.googleapis.com/css2?family=Muli:wght@400;700&family=Roboto:wght@300;500&display=swap" rel="stylesheet"></link>
+
+            <title>{ title } | Fachschaft IWI</title>
+      </Head>
+    )
+}
+
+export default PageHead
