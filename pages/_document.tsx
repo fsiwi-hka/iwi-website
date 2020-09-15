d5bab5e9e0ae2b52777fa5725bbdd9d942fece44
Christian Wernet <github@cwernet.de>
2020-09-15 20:37:43 +0200

    Fix spelling
    Aaand there is the second 'c' 😝

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      // we set the lang attribute here for acessibility
+      // we set the lang attribute here for accessibility
       <Html lang="de">
         <Head />
         <body>
