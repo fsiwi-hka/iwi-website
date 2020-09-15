a328ea160bc7eb00e81a8e537efa0ff95a748a89
Simon Kienzler <SimonKienzler@users.noreply.github.com>
2020-09-15 14:44:31 +0200

    Add lang attribute to html tag

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+/* This document is needed because it's the simplest way to alter
+ * surrounding HTML tags like <html> and <body>. See the documentation:
+ * 
+ * https://nextjs.org/docs/advanced-features/custom-document
+ */
+
+class MyDocument extends Document {
+  render() {
+    return (
+      // we set the lang attribute here for acessability
+      <Html lang="de">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
